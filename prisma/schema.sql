ee17fd718a4eeacde387237415edbbc6ac4688a5
hoosa <janghoosa@gmail.com>
2023-02-19 22:33:26 +0900

    feat: task, dashboard, panel 스키마 추가

--- prisma/schema.sql
+++ prisma/schema.sql
@@ -44,6 +44,34 @@ CREATE TABLE `script` (
 	`updated_date`	datetime NOT NULL ON UPDATE CURRENT_TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
 
+DROP TABLE IF EXISTS `task`;
+CREATE TABLE `task` (
+	`task_id`	int	primary key auto_increment,
+	`info`	varchar(255)	NOT NULL,
+	`is_complete`	tinyint	NOT NULL,
+	`created_date`	datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+	`updated_date`	datetime NOT NULL ON UPDATE CURRENT_TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+
+DROP TABLE IF EXISTS `dashboard`;
+CREATE TABLE `dashboard` (
+	`dashboard_id`	int	primary key auto_increment,
+	`name`	varchar(255)	NOT NULL,
+	`uid`	varchar(255)	NOT NULL,
+	`url`	varchar(500)	NOT NULL
+);
+
+DROP TABLE IF EXISTS `panel`;
+CREATE TABLE `panel` (
+	`panel_id`	int	primary key auto_increment,
+	`uid`	varchar(255)	NOT NULL,
+	`x`	int	NOT NULL,
+	`y`	int	NOT NULL,
+	`z`	int	NOT NULL,
+	`h`	int	NOT NULL,
+	`mode`	varchar(255)	NOT NULL
+);
+
 ALTER TABLE `edge` ADD CONSTRAINT `FK_node_TO_edge_1` FOREIGN KEY (
 	`node_id`
 )
